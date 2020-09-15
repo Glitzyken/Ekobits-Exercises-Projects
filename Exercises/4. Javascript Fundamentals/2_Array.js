5c9dcd691544ef097c8dbbb4a2855c46bbcc4a13
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-09-15 18:34:26 +0100

    reorganize things

---
+++ Exercises/4. Javascript Fundamentals/2_Array.js
@@ -0,0 +1,18 @@
+// Array Exercises 1 - 11
+
+// 1
+let people = ["Greg", "Mary", "Devon", "James"];
+
+// for (let i = 0; i < people.length; i++) {
+//   console.log(people[i]);
+// }
+
+// 2
+// people.splice(0, 1);
+
+// console.log(people);
+
+// 3
+people.splice(3, 1);
+
+console.log(people);
