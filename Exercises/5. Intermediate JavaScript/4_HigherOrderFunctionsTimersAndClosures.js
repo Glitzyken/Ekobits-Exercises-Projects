9b7aeb3356e8bb9476e88ce2be909fe22c452bb3
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-10-08 22:52:13 +0100

    done

---
+++ Exercises/5. Intermediate JavaScript/4_HigherOrderFunctionsTimersAndClosures.js
@@ -0,0 +1,16 @@
+// 1)  countdown
+// Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds decrements the value and console.logs it. Once the value is 0 it should log "DONE!" and stop.
+
+const countdown = (num) => {
+  let makeNumArr = [];
+
+  for (let i = 0; i < num; i++) {
+    makeNumArr.push(i);
+
+    setTimeout(function () {
+      console.log(makeNumArr.pop());
+    }, i * 1000);
+  }
+};
+
+countdown(4);
