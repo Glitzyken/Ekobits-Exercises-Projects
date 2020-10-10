3445802dbd087d21b55056fc87b55512dc58382d
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-10-10 13:14:24 +0100

    done isEven

--- Exercises/5. Intermediate JavaScript/4_HigherOrderFunctionsTimersAndClosures.js
+++ Exercises/5. Intermediate JavaScript/4_HigherOrderFunctionsTimersAndClosures.js
@@ -26,23 +26,37 @@
 // 2) randomGame
 // Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, stop the timer and return the number of tries it took before we found a number greater than .75.
 
-const randomGame = () => {
-  // declare a counter variable and set it to 0 initially
-  // every 1 sec, check if random number is greater than .75.
-  // if false, increase counter by 1.
-  // if true, stope timer and return the number of tries
+// const randomGame = () => {
+//   // declare a counter variable and set it to 0 initially
+//   // every 1 sec, check if random number is greater than .75.
+//   // if false, increase counter by 1.
+//   // if true, stope timer and return the number of tries
 
-  let counter = 0;
+//   let counter = 0;
 
-  let loop = setInterval(() => {
-    if (Math.round(Math.random() * 100) / 100 > 0.75) {
-      console.log("number of tries is " + counter);
-      clearInterval(loop);
-    } else {
-      console.log("Added 1 to counter");
-      counter += 1;
-    }
-  }, 1000);
+//   let loop = setInterval(() => {
+//     if (Math.round(Math.random() * 100) / 100 > 0.75) {
+//       console.log("number of tries is " + counter);
+//       clearInterval(loop);
+//     } else {
+//       console.log("Added 1 to counter");
+//       counter += 1;
+//     }
+//   }, 1000);
+// };
+
+// randomGame();
+
+// 3) isEven
+// Write a function called isEven which takes in a number and returns true if the number is even and returns false if it is not
+
+const isEven = (num) => {
+  if (num % 2 === 0) {
+    console.log('True')
+  } else {
+    console.log('False')
+  }
 };
 
-randomGame();
+isEven(2); // true
+isEven(3); // false
