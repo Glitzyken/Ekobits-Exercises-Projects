70281d478dfb0b40672ed2e4934a4920611e1aeb
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-10-10 13:01:54 +0100

    finish number 2

--- Exercises/5. Intermediate JavaScript/4_HigherOrderFunctionsTimersAndClosures.js
+++ Exercises/5. Intermediate JavaScript/4_HigherOrderFunctionsTimersAndClosures.js
@@ -1,19 +1,48 @@
 // 1)  countdown
 // Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds decrements the value and console.logs it. Once the value is 0 it should log "DONE!" and stop.
 
-const countdown = (num) => {
-  let makeNumArr = [];
+// const countdown = (num) => {
+// initialize an empty array
+// loop over num
+// push value at each iterarion to empty array
+// use setTimeout to log values of empty array to console
 
-  for (let i = 0; i < num; i++) {
-    makeNumArr.push(i);
-    if (i === 0) {
-      makeNumArr.push('Done!')
+//   let makeNumArr = [];
+
+//   for (let i = 0; i < num; i++) {
+//     makeNumArr.push(i);
+//     if (i === 0) {
+//       makeNumArr.push('Done!')
+//     }
+
+//     setTimeout(function () {
+//       console.log(makeNumArr.pop());
+//     }, i * 1000);
+//   }
+// };
+
+// countdown(4);
+
+// 2) randomGame
+// Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, stop the timer and return the number of tries it took before we found a number greater than .75.
+
+const randomGame = () => {
+  // declare a counter variable and set it to 0 initially
+  // every 1 sec, check if random number is greater than .75.
+  // if false, increase counter by 1.
+  // if true, stope timer and return the number of tries
+
+  let counter = 0;
+
+  let loop = setInterval(() => {
+    if (Math.round(Math.random() * 100) / 100 > 0.75) {
+      console.log("number of tries is " + counter);
+      clearInterval(loop);
+    } else {
+      console.log("Added 1 to counter");
+      counter += 1;
     }
-
-    setTimeout(function () {
-      console.log(makeNumArr.pop());
-    }, i * 1000);
-  }
+  }, 1000);
 };
 
-countdown(4);
+randomGame();
