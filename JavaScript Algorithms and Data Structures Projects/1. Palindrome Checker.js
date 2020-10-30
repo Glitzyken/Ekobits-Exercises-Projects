181254f87cd251ab829529c3ad408d31259af886
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-10-30 09:34:36 +0100

    palindrome

--- JavaScript Algorithms and Data Structures Projects/1. Palindrome Checker.js
+++ JavaScript Algorithms and Data Structures Projects/1. Palindrome Checker.js
@@ -1,27 +1,32 @@
 function palindrome(str) {
-  //assign a front and a back pointer
+  // assign a front and a back pointer
+  // increment front pointer if current character doesn't meet criteria
+  // decrements back pointer if current character doesn't meet criteria
+  // compare the current characters (front and back)
+  // if the whole string has been compared without returning false, it's a palindrome!
+
   let front = 0;
   let back = str.length - 1;
 
-  //back and front pointers won't always meet in the middle, so use (back > front)
+  // since back and front pointers won't always meet in the middle, use (back > front)
   while (back > front) {
-    //increments front pointer if current character doesn't meet criteria
     if (str[front].match(/[\W_]/)) {
+      // regex will match any non-word char or _
+      // this will make sure to ignore all non-alphanumeric characters
       front++;
       continue;
     }
-    //decrements back pointer if current character doesn't meet criteria
+
     if (str[back].match(/[\W_]/)) {
       back--;
       continue;
     }
-    //finally does the comparison on the current character
+
     if (str[front].toLowerCase() !== str[back].toLowerCase()) return false;
     front++;
     back--;
   }
 
-  //if the whole string has been compared without returning false, it's a palindrome!
   return true;
 }
 
