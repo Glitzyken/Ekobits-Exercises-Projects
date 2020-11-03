499496c0d4180f004333c154f2239ba68ffbcf4e
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-11-03 14:39:01 +0100

    challenge 2

--- 2. Number of drawn matches.js
+++ 2. Number of drawn matches.js
@@ -1,27 +1,41 @@
 async function getNumDraw(year) {
-  const url = `https://jsonmock.hackerrank.com/api/football_matches?year=${year}&page=1`;
+  const drawnMatchesArray = [];
 
-  await fetch(url)
+  const totalPages = await fetch(
+    `https://jsonmock.hackerrank.com/api/football_matches?year=${year}`
+  )
     .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      return data.data;
-    })
-    .then((dataArr) => {
-      let count = 0;
-      dataArr.forEach((el) => {
-        let team1goal = el.team1goals;
-        let team2goal = el.team2goals;
+    .then((data) => data.total_pages); // 196
 
-        // console.log(el);
+  for (let i = 1; i <= totalPages; i++) {
+    let url = `https://jsonmock.hackerrank.com/api/football_matches?year=${year}&page=${i}`;
 
-        if (team1goal === team2goal) {
-          count += 1;
-        }
+    await fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        return data.data;
+      })
+      .then((dataArr) => {
+        let count = 0;
+        dataArr.forEach((el) => {
+          let team1goal = el.team1goals;
+          let team2goal = el.team2goals;
+
+          if (team1goal === team2goal) {
+            count += 1;
+          }
+        });
+
+        console.log(`Fetched ${count} drawn matches from page ${i}`); // This for a visible sign of progress
+        drawnMatchesArray.push(count);
       });
+  }
 
-      console.log(count);
-    });
+  const totalResultOfDrawnMatches = drawnMatchesArray.reduce(
+    (acc, val) => acc + val
+  );
+
+  console.log(totalResultOfDrawnMatches);
 }
 
 getNumDraw(2011);
