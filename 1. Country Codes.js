1dd8671315a350a88262b5c579da4032a3e79c65
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-11-07 20:20:30 +0100

    finish challenge 1

--- 1. Country Codes.js
+++ 1. Country Codes.js
@@ -1,5 +1,31 @@
-function getCountryName(code) {
-  const url = 'https://jsonmock.hackerrank.com/api/countries?page=<pageNumber>';
+async function getCountryName(code) {
+  let filteredCountryName;
+
+  const totalPages = await fetch(
+    'https://jsonmock.hackerrank.com/api/countries?page=1'
+  )
+    .then((res) => res.json())
+    .then((data) => data.total_pages); // 25
+
+  for (let i = 1; i <= totalPages; i++) {
+    let url = `https://jsonmock.hackerrank.com/api/countries?page=${i}`;
+
+    await fetch(url)
+      .then((res) => res.json())
+      .then((data) => data.data)
+      .then((dataArray) => {
+        dataArray.filter((countryObj) => {
+          if (code === countryObj.alpha2Code) {
+            console.log('Found!');
+            filteredCountryName = countryObj.name;
+            return;
+          }
+        });
+      });
+  }
+
+  console.log(filteredCountryName);
 }
 
-getCountryName('ZW');
+// getCountryName('ZW');
+getCountryName('AF');
