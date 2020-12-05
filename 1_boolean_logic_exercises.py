6fbf91cc75b9e7ee26bf7caae7ddc335ad30f8c6
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-12-05 09:12:37 +0100

    finish boolean logic exercise

---
+++ 1_boolean_logic_exercises.py
@@ -0,0 +1,45 @@
+first = "Hello World"
+
+# This is a comment.
+
+print('I AM A COMPUTER!')
+
+if 1 < 2 and 4 > 2:
+    print("Math is fun.")
+
+nope = None
+
+print(len("What's my length?"))
+print("i am shouting".upper())
+
+print(int('1000'))
+
+print(str(4) + 'real')
+
+record1 = 3 * 'cool'
+print(record1)
+
+# record2 = 1 / 0
+# print(record2)
+
+print(type([]))
+
+name = input('Enter your name: ')
+print(name)
+
+user_num = int(input('Eneter your number: '))
+if user_num < 0:
+    print("That number is less than 0!")
+elif user_num > 0:
+    print("That number is greater than 0!")
+else:
+    print("You picked 0!")
+
+print("apple".index('p'))
+
+if 'x' in "xylophone":
+    print('yes')
+
+my_string = "my_string"
+if my_string.lower():
+    print('is_lower_case')
